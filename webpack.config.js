874c376d24cd6e4f38afa80ff4ee1579053c2de7
Alejo Taliento <alee.taliento@gmail.com>
2021-09-14 16:08:49 -0300

    (add): plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "development",
@@ -26,12 +27,19 @@ module.exports = {
         test: /\.html$/,
         use: [{ loader: "html-loader" }],
       },
+      {
+        test: /\.s[ac]ss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./public/index.html",
       filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
     }),
   ],
   devServer: {
